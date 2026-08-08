cbfdfdc35d25504797b088c155f06cec9a08f8b4
MAHESHPOORAB3001 <poorabmahesh3001@gmail.com>
2026-08-08 14:10:38 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k=0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k=k+1
+        return k 
+
+    """i=0
+        n=len(nums)
+        while i<n:
+            if nums[i] == val:
+                n=n-1
+                nums[i]=nums[n]
+            else:
+                i=i+1
+        return n """
+
+
+
+
+            
+            
+
+        
